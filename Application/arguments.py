dc0981b481075511ef0eb904505fa3db1ae4ead5
cendrars59 <cyr.leroux.dev@moinslequart.net>
2021-10-12 21:47:24 +0200

    Project initialization

---
+++ Application/arguments.py
@@ -0,0 +1,5 @@
+"""
+Managing the application arguments.
+"""
+
+import argparse
---
+++ starter.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+"""
+    Author : Cyril Leroux <fakeemail@totor.com>
+    Date : [Today]
+    Purpose : It is a boilerplate for command line application
+"""
+
+
+def main():
+    """[summary]
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
